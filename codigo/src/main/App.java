2121626883e89659009fa2c8938a73c536b68aed
Andryws121 <130196791+Andryws121@users.noreply.github.com>
2024-10-09 21:26:08 -0300

    Update App.java

--- codigo/src/main/App.java
+++ codigo/src/main/App.java
@@ -3,21 +3,5 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
 
-        Scanner scanner = new Scanner;
-        int a, b, c;
-
-        System.out.println("Vamos Somar!");
-        System.out.println("Digite o numero 1: "\n);
-
-        a = scanner.nextLine();
-
-        System.out.println("Digite o numero 2: "\n);
-
-        b = scanner.nextLine();
-
-        c = a + b;
-
-        System.out.println("A soma é : " + c);
-
     }
 }
